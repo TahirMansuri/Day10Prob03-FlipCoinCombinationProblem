a7271331d4d2570649f4c75aa4e3253738d7f270
TahirMansuri <tprocks786@gmail.com>
2021-11-28 16:15:36 +0530

    MODIFIED Fet Doublet of HeadTail and Percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,12 +3,17 @@
 echo "Welcome to  Flip Coin Combination Program";
 
 # Creating Dictionary for Head and Tail
-declare -A HeadDict
-declare -A TailDict
+declare -A DoubletDict
 
 # Variables
-headCount=0;
-tailCount=0;
+n=0;
+F="";
+index=1;
+
+HHCount=0;
+TTCount=0;
+HTCount=0;
+THCount=0;
 
 # Flip Coin to get Head and Tail
 for (( i=1; i<=20; i++ ))
@@ -16,18 +21,46 @@ do
 	flip=$((RANDOM%2));
 	case $flip in
 		0)
-			((headCount++));
-			HeadDict[$headCount]=$i;
-			echo "Head Comes";
+			F=$F"H";
 		;;
 		1)
-			((tailCount++));
-			TailDict[$tailCount]=$i;
-			echo "Tail Comes";
+			F=$F"T";
 		;;
 	esac
+	((n++));
+	if (( n==2 ))
+	then
+		DoubletDict[$index]=$F;
+		n=0;
+
+		case $F in
+			'HH')
+				((HHCount++));
+			;;
+			'TT')
+				((TTCount++));
+			;;
+			'TH')
+				((THCount++));
+			;;
+			'HT')
+				((HTCount++));
+			;;
+		esac
+
+		F="";
+		((index++));
+	fi
+
 done
 
-#Getting Percentage of Head and Tail Occurance
-echo "Head Percentage="$(( (${#HeadDict[@]} / 2) * 10 ));
-echo "Tail Percentage="$(( (${#TailDict[@]} / 2) * 10 ));
+#Getting Percentage of Head and Tail Double Occurance
+echo "HH Percentage="$(( $HHCount * 10 ));
+echo "TT Percentage="$(( $TTCount * 10 ));
+echo "TH Percentage="$(( $THCount * 10 ));
+echo "HT Percentage="$(( $HTCount * 10 ));
+
+for key in ${!DoubletDict[@]}
+do
+	echo $key " : " ${DoubletDict[$key]};
+done
